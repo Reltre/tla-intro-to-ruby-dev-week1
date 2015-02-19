d7b5b09db2879c2d89adca489ee50d54f119e699
Reltre <tglippert@gmail.com>
2015-02-19 14:22:33 -0800

    calculator app completed

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,44 @@
-def my_method(arr)
-  arr.uniq!
+def options
+  puts "Please enter a number use in the calculator."
+  num1 = gets.chomp 
+  num1 = gets.chomp until num1.to_f.is_a?(Float) && num1.to_f != 0.0
+
+  puts "Please enter another number use in the calculator."
+  num2 = gets.chomp 
+  num2 = gets.chomp until num2.to_f.is_a?(Float) && num2.to_f != 0.0
+
+  puts "Now, enter an operator to use, enter either '+','-','*',or '/'.",
+  "You may also enter 'quit' at this point to exit the program."
+  operation = gets.chomp   
+
+  return num1, num2, operation
 end
 
-my_arr = [1, 2, 2, 3]
-my_method(my_arr)
-puts my_arr # => was the outer scope array affected by the method invocation?
+
+
+
+
+
+loop do
+
+  num1 ,num2,operation = options
+
+  case operation
+  when '+'
+    puts "#{num1} - #{num2} = #{num1.to_f - num2.to_f}"
+  when '-'
+    puts "#{num1} - #{num2} = #{num1.to_f - num2.to_f}"
+  when '*'
+    puts "#{num1} * #{num2} = #{num1.to_f * num2.to_f}"
+  when '/'
+    (num2 != "0") ?  puts("#{num1} / #{num2} = #{num1.to_f / num2.to_f}") : 
+    puts("Dividing by zero is not a valid operation")
+  when 'quit'
+    break
+  else
+    puts "That is not a valid operator."
+  end
+
+end
+
+puts "Thank you for using this calculator!"
